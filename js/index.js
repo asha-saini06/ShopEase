6b2491ebd032d484278c86b0a338139a684e5856
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-02-11 15:25:23 +0530

    innerHTML to append li to ul

--- js/index.js
+++ js/index.js
@@ -10,25 +10,32 @@ const appSettings = {
   databaseURL:
     "https://shopease-238e6-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
+const app = initializeApp(appSettings);
 
-const app = initializeApp(appSettings); // Initialize Firebase
 const database = getDatabase(app); // Database reference
 const shoppingListInDB = ref(database, "products"); // Reference to the "products" node in the database
 
-// Wait for the DOM content to be loaded before adding event listeners
 document.addEventListener("DOMContentLoaded", function () {
-  // Get references to the input field and the add button
   const inputField = document.getElementById("input-field");
   const addButton = document.getElementById("add-btn");
+  const shoppingList = document.getElementById("shopping-list");
 
-  // Add event listener to the add button
   addButton.addEventListener("click", function () {
-    // Retrieve the value from the input field
     let inputValue = inputField.value;
+    push(shoppingListInDB, inputValue);
+    console.log(inputValue + " added to the list");
 
-    push(shoppingListInDB, inputValue); // Add the input value to the database
+    // Clear input field
+    inputField.value = "";
 
-    // Log the input value to the console
-    console.log(inputValue + " added to database.");
+    // Add new item to the list
+    shoppingList.innerHTML += `<li>${inputValue}</li>`;
+  });
+
+  // add .strike class to li items when clicked on them -->
+  shoppingList.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("strike");
+    }
   });
 });
