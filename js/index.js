35013c6139294767c00a3fb932e96b4a79cc739e
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-02-13 19:58:00 +0530

    Updating items in realtime

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
 
 // Initialize Firebase
@@ -10,32 +11,51 @@ const appSettings = {
   databaseURL:
     "https://shopease-238e6-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
-const app = initializeApp(appSettings);
 
+const app = initializeApp(appSettings);
 const database = getDatabase(app); // Database reference
 const shoppingListInDB = ref(database, "products"); // Reference to the "products" node in the database
 
-document.addEventListener("DOMContentLoaded", function () {
-  const inputField = document.getElementById("input-field");
-  const addButton = document.getElementById("add-btn");
-  const shoppingList = document.getElementById("shopping-list");
+const inputField = document.getElementById("input-field");
+const addButton = document.getElementById("add-btn");
+const shoppingList = document.getElementById("shopping-list");
 
-  addButton.addEventListener("click", function () {
-    let inputValue = inputField.value;
-    push(shoppingListInDB, inputValue);
-    console.log(inputValue + " added to the list");
+onValue(shoppingListInDB, function (snapshot) {
+  let productsArray = Object.values(snapshot.val());
+  clearShoppingList(); // Clear the list before adding new items
 
-    // Clear input field
-    inputField.value = "";
+  // for loop to console log each product
+  for (let i = 0; i < productsArray.length; i++) {
+    let currentProduct = productsArray[i]; // Get the current product
+    addToList(currentProduct); // Add each product to the list
+  }
+});
 
-    // Add new item to the list
-    shoppingList.innerHTML += `<li>${inputValue}</li>`;
-  });
+function clearShoppingList() {
+  shoppingList.innerHTML = "";
+}
 
-  // add .strike class to li items when clicked on them -->
-  shoppingList.addEventListener("click", function (event) {
-    if (event.target.tagName === "LI") {
-      event.target.classList.toggle("strike");
-    }
-  });
+addButton.addEventListener("click", function () {
+  let inputValue = inputField.value;
+
+  push(shoppingListInDB, inputValue);
+
+  clearList();
+});
+
+function clearList() {
+  inputField.value = "";
+}
+
+function addToList(itemValue) {
+  // Add new item to the list
+  shoppingList.innerHTML += `<li>${itemValue}</li>`;
+  // console.log("Item added to the list:", itemValue);
+}
+
+// add .strike class to li items when clicked on them
+shoppingList.addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("strike");
+  }
 });
