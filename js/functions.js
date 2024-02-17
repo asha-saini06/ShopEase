d70717c49c853db9c5d4a74916e829de57103c85
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-02-17 22:55:13 +0530

    Making the app more mobile-friendly + Web Application Manifest

--- js/functions.js
+++
@@ -1,9 +0,0 @@
-let scramble = {
-  "00": "abc@gmail.com",
-  "01": "xyz@gmail.com",
-  "02": "pqr@gmail.com",
-};
-
-// console.log(Object.values(scramble));
-// console.log(Object.keys(scramble));
-// console.log(Object.entries(scramble));
